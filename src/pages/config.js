4eeacd395cb7cf338854124bc968f9ced233b81c
yanling.liao <yanling.liao@elaine.liao>
2018-10-29 19:15:22 +0800

    feat: add configjs

---
+++ src/pages/config.js
@@ -0,0 +1,54 @@
+export default {
+  priceList: [
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '富油洗沙芝麻核桃酥叶儿粑',
+      price: 2.5
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '鲜肉芽菜叶儿粑',
+      price: 2.5
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '甜皮咸心叶儿粑',
+      price: 2.5
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '花生芝麻白糖叶儿粑',
+      price: 2.5
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '小叶儿粑',
+      price: 1.5
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '富油泡粑',
+      price: 1
+    },
+    {
+      image: '../assets/WechatIMG49.jpeg',
+      name: '富油小泡粑',
+      price: 0.5
+    }
+  ],
+  // 图片预览列表
+  imageList: [
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG30.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG17.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG12.jpg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG13.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG15.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG18.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG19.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG21.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG22.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG23.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG29.jpeg',
+    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG32.jpeg'
+  ]
+}
