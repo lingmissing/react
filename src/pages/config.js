cb5a86a68f83cd62ce6fdecb6cfd4c2805a62012
yanling.liao <yanling.liao@elaine.liao>
2018-10-30 15:54:08 +0800

    add image

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,54 +1,54 @@
 export default {
   priceList: [
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb19.jpeg',
       name: '富油洗沙芝麻核桃酥叶儿粑',
       price: 2.5
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb21.jpeg',
       name: '鲜肉芽菜叶儿粑',
       price: 2.5
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb26.jpeg',
       name: '甜皮咸心叶儿粑',
       price: 2.5
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb25.jpeg',
       name: '花生芝麻白糖叶儿粑',
       price: 2.5
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb27.jpeg',
       name: '小叶儿粑',
       price: 1.5
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb25.jpeg',
       name: '富油泡粑',
       price: 1
     },
     {
-      image: '../assets/WechatIMG49.jpeg',
+      image: 'yeb24.jpeg',
       name: '富油小泡粑',
       price: 0.5
     }
   ],
   // 图片预览列表
   imageList: [
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG30.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG17.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG12.jpg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG13.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG15.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG18.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG19.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG21.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG22.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG23.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG29.jpeg',
-    'https://raw.githubusercontent.com/lingmissing/yeb/master/src/assets/WechatIMG32.jpeg'
+    'WechatIMG30.jpeg',
+    'WechatIMG17.jpeg',
+    'WechatIMG12.jpg',
+    'WechatIMG13.jpeg',
+    'WechatIMG15.jpeg',
+    'WechatIMG18.jpeg',
+    'WechatIMG19.jpeg',
+    'WechatIMG21.jpeg',
+    'WechatIMG22.jpeg',
+    'WechatIMG23.jpeg',
+    'WechatIMG29.jpeg',
+    'WechatIMG32.jpeg'
   ]
 }
