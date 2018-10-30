0577faef1dfdd47555b3d1ebb14f9d2fed2bce88
yanling.liao <yanling.liao@elaine.liao>
2018-10-30 16:07:08 +0800

    rename

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,54 +1,39 @@
 export default {
   priceList: [
     {
-      image: 'yeb19.jpeg',
+      image: 'yeb19.jpg',
       name: '富油洗沙芝麻核桃酥叶儿粑',
       price: 2.5
     },
     {
-      image: 'yeb21.jpeg',
+      image: 'yeb21.jpg',
       name: '鲜肉芽菜叶儿粑',
       price: 2.5
     },
     {
-      image: 'yeb26.jpeg',
+      image: 'yeb25.jpg',
       name: '甜皮咸心叶儿粑',
       price: 2.5
     },
     {
-      image: 'yeb25.jpg',
+      image: 'none.jpg',
       name: '花生芝麻白糖叶儿粑',
       price: 2.5
     },
     {
-      image: 'yeb25.jpg',
+      image: 'none.jpg',
       name: '小叶儿粑',
       price: 1.5
     },
     {
-      image: 'yeb27.jpeg',
+      image: 'yeb26.jpg',
       name: '富油泡粑',
       price: 1
     },
     {
-      image: 'yeb24.jpeg',
+      image: 'yeb24.jpg',
       name: '富油小泡粑',
       price: 0.5
     }
-  ],
-  // 图片预览列表
-  imageList: [
-    'WechatIMG30.jpeg',
-    'WechatIMG17.jpeg',
-    'WechatIMG12.jpg',
-    'WechatIMG13.jpeg',
-    'WechatIMG15.jpeg',
-    'WechatIMG18.jpeg',
-    'WechatIMG19.jpeg',
-    'WechatIMG21.jpeg',
-    'WechatIMG22.jpeg',
-    'WechatIMG23.jpeg',
-    'WechatIMG29.jpeg',
-    'WechatIMG32.jpeg'
   ]
 }
